b932840e3e8a68b79133e7dd2f5876344d3f574d
JiaHualong <magic.hualong@gmail.com>
2017-09-24 13:11:17 +0800

    merge zh en

--- gen-ankicard.scala
+++ gen-ankicard.scala
@@ -2,8 +2,9 @@
 exec scala "$0" "$@"
 !#
 
+import java.io.FileWriter
+
 import scala.io.Source
-import java.io.{File, FileWriter}
 
 if (args.length != 1) {
   println("usage: gen-ankicard.scala macmillan7000word.list")
@@ -13,31 +14,30 @@ if (args.length != 1) {
 var wordDirMerge = "wordmerge"
 var ankicardName = "macmillan7000word.card.txt"
 
-var ankicard = new FileWriter(ankicardName,false)
+var ankicard = new FileWriter(ankicardName, false)
 
 Source.fromFile(args(0)).getLines().toList.foreach(word => {
 
   var lines = Source.fromFile(wordDirMerge + "/" + word + ".txt").getLines().toList
 
-  var cardword = lines(0).replaceAll("\n| |\t","")
-  var cardsymbol = lines(1).replaceAll("\n| |\t","")
-  var cardsound = "[sound:macmillan7000_" + word + ".mp3]".replaceAll("\n| |\t","")
+  if (lines.length >= 3) {
 
-  //正面
-  ankicard.write("\"" + cardword + "\"\t")
-  ankicard.write("\"" + cardsymbol + "\"\t")
-  ankicard.write("\"" + cardsound  + "\"\t")
-
-
-  //反面
-  ankicard.write("\"")
-
-  lines.drop(2).foreach( e => {
-    ankicard.write(e + "<br />")
-  })
-
-  ankicard.write("\"\n")
-
+//    var cardword = lines(0).replaceAll("\n| |\t", "")
+//    var cardsymbol = lines(1).replaceAll("\n| |\t", "")
+//    var cardsound = "[sound:macmillan7000_" + word + ".mp3]".replaceAll("\n| |\t", "")
+//
+//    //正面
+//    ankicard.write("\"" + cardword + "\"\t")
+//    ankicard.write("\"" + cardsymbol + "\"\t")
+//    ankicard.write("\"" + cardsound + "\"\t")
+//
+//
+//    //反面
+//    ankicard.write("\"")
+//
+  } else {
+    println(word)
+  }
 
 })
 
