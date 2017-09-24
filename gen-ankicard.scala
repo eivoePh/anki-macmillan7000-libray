1d71edb211a57e69aa18b7ec63c6830f25c7793a
JiaHualong <magic.hualong@gmail.com>
2017-09-24 16:08:59 +0800

    生成ankicard时扔到第二行可能出现的 "1 "

--- gen-ankicard.scala
+++ gen-ankicard.scala
@@ -22,21 +22,32 @@ Source.fromFile(args(0)).getLines().toList.foreach(word => {
 
   if (lines.length >= 3) {
 
-//    var cardword = lines(0).replaceAll("\n| |\t", "")
-//    var cardsymbol = lines(1).replaceAll("\n| |\t", "")
-//    var cardsound = "[sound:macmillan7000_" + word + ".mp3]".replaceAll("\n| |\t", "")
-//
-//    //正面
-//    ankicard.write("\"" + cardword + "\"\t")
-//    ankicard.write("\"" + cardsymbol + "\"\t")
-//    ankicard.write("\"" + cardsound + "\"\t")
-//
-//
-//    //反面
-//    ankicard.write("\"")
-//
+    var cardword = lines(0).replaceAll("\n| ", "")
+    var cardsymbol = lines(1).replaceAll("\n", "")
+
+    if (cardsymbol == "1 ") {
+      cardsymbol = lines(2).replaceAll("\n", "")
+      lines = lines.drop(3) //第二行可能是 "1 "移除掉
+    } else {
+      lines = lines.drop(2)
+    }
+
+    var cardsound = "[sound:macmillan7000_" + word + ".mp3]"
+
+    //正面
+    ankicard.write("\"" + cardword + "<span style=\"\"display:none\"\">macmillan7000</span>\"\t")
+    ankicard.write("\"" + cardsymbol + "\"\t")
+    ankicard.write("\"" + cardsound + "\"\t")
+
+    //反面
+    ankicard.write("\"")
+    lines.foreach(line => {
+      if (line.length > 0)
+        ankicard.write(line + "<br />")
+    })
+    ankicard.write("\"\n")
   } else {
-    println(word)
+    println("这个单词卡不足3行: " + word)
   }
 
 })
